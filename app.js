4faa89726a08474ebf7219f4d747d9db971f1a97
David Horès <hores.d@gmail.com>
2024-08-19 11:14:24 +0200

    Add middlewares with use and post methods to Express app and update old one with get method in app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -15,7 +17,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/books", (req, res, next) => {
+app.post("/api/books", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !"
+  });
+});
+
+app.get("/api/books", (req, res, next) => {
   const books = [
     {
       _id: "oeihfzeoi",

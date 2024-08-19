415f724c2ebfba3d08f952ebbb3aa6d1e2eed64f
David Horès <hores.d@gmail.com>
2024-08-19 11:30:10 +0200

    Install Mongoose dependency and use it in app.js with connect, then and catch methods to connect API to MongoDB cluster

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://dave25440:grimoire@cluster0.t47em.mongodb.net/grimoire?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,6 +23,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use(express.json());
 
 app.post("/api/books", (req, res, next) => {
   console.log(req.body);

9304296ebb557d349ae46d9cdba3e461bcba051c
David Horès <hores.d@gmail.com>
2024-08-19 10:56:37 +0200

    Remove app middlewares and add new one with books endpoint and array in app.js

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
+app.use("/api/books", (req, res, next) => {
+  const books = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios"
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios"
+    }
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;

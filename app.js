165bc14978071aeed3a7ccd15dae8b3424a9fad7
David Horès <hores.d@gmail.com>
2024-08-19 12:33:16 +0200

    Add middleware with get method and books/:id endpoint to app.js and use findOne, then and catch methods to retrieve book from database whose id matches query id

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.post("/api/books", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+app.get("/api/books/:id", (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error }));
+});
+
 app.get("/api/books", (req, res, next) => {
   Book.find()
     .then((books) => res.status(200).json(books))

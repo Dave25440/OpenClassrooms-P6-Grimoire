f2f5ab71111242fb6f6857d9be22d8f8b9d98eb9
David Horès <hores.d@gmail.com>
2024-08-22 13:32:36 +0200

    Install JSON Web Token dependency and add it to user controller to create authentication token with user id, secret string and validity time in login function

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -36,7 +37,9 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: "TOKEN"
+                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                  expiresIn: "24h"
+                })
               });
             }
           })

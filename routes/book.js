c0972e6431b76b07f913a64cae5bc7b1b10a4c90
David Horès <hores.d@gmail.com>
2024-08-22 13:52:02 +0200

    Create authentication middleware in new auth.js file and new middleware folder, add JSON Web Token dependency to it to verify token validity and add it to book routes

--- routes/book.js
+++ routes/book.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
+
 const bookCtrl = require("../controllers/book");
 
-router.post("/", bookCtrl.createBook);
-router.put("/:id", bookCtrl.modifyBook);
-router.delete("/:id", bookCtrl.deleteBook);
-router.get("/:id", bookCtrl.getOneBook);
-router.get("/", bookCtrl.getAllBooks);
+router.post("/", auth, bookCtrl.createBook);
+router.put("/:id", auth, bookCtrl.modifyBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
+router.get("/:id", auth, bookCtrl.getOneBook);
+router.get("/", auth, bookCtrl.getAllBooks);
 
 module.exports = router;

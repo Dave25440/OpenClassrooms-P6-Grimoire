11d9aa20bd23dca71d27cce3f6c974cc2dfa5b21
David Horès <hores.d@gmail.com>
2024-08-22 11:28:36 +0200

    Create book.js file in new controllers folder, move all book functions to it and add it to book routes

--- routes/book.js
+++ routes/book.js
@@ -1,41 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const Book = require("../models/book");
+const bookCtrl = require("../controllers/book");
 
-router.post("/", (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-router.put("/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-router.delete("/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-router.get("/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-router.get("/", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.post("/", bookCtrl.createBook);
+router.put("/:id", bookCtrl.modifyBook);
+router.delete("/:id", bookCtrl.deleteBook);
+router.get("/:id", bookCtrl.getOneBook);
+router.get("/", bookCtrl.getAllBooks);
 
 module.exports = router;

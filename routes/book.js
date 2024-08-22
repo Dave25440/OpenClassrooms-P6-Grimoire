6f46fc4a716baa36346e5192f2e4988bc8a476a3
David Horès <hores.d@gmail.com>
2024-08-22 14:06:21 +0200

    Remove auth middleware from books' GET routes in routes/book.js

--- routes/book.js
+++ routes/book.js
@@ -8,7 +8,7 @@ const bookCtrl = require("../controllers/book");
 router.post("/", auth, bookCtrl.createBook);
 router.put("/:id", auth, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
-router.get("/:id", auth, bookCtrl.getOneBook);
-router.get("/", auth, bookCtrl.getAllBooks);
+router.get("/:id", bookCtrl.getOneBook);
+router.get("/", bookCtrl.getAllBooks);
 
 module.exports = router;

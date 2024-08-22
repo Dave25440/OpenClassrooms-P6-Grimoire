d39c24a0c2fb3101522d08edd0f1a1a7e9408cec
David Horès <hores.d@gmail.com>
2024-08-22 13:10:23 +0200

    Add findOne and compare methods to user controller's login function to check wether email and password entered by user match those stored in database

--- controllers/user.js
+++ controllers/user.js
@@ -18,4 +18,30 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Paire email/mot de passe incorrecte" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: "Paire email/mot de passe incorrecte" });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: "TOKEN"
+              });
+            }
+          })
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};

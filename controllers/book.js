2a6b074ef36390f6557b1ce312b4f9bce87ae6e5
David Horès <hores.d@gmail.com>
2024-08-27 11:59:31 +0200

    Update modifyBook function in book controller to remove book image from file system using unlink method in fs package when user uploads new image

--- controllers/book.js
+++ controllers/book.js
@@ -85,12 +85,25 @@ exports.modifyBook = (req, res, next) => {
       if (book.userId != req.auth.userId) {
         res.status(403).json({ message: "Requête non autorisée" });
       } else {
-        Book.updateOne(
-          { _id: req.params.id },
-          { ...bookObject, _id: req.params.id }
-        )
-          .then(() => res.status(200).json({ message: "Livre modifié" }))
-          .catch((error) => res.status(401).json({ error }));
+        if (req.file) {
+          const filename = book.imageUrl.split("/images/")[1];
+
+          fs.unlink(`images/${filename}`, () => {
+            Book.updateOne(
+              { _id: req.params.id },
+              { ...bookObject, _id: req.params.id }
+            )
+              .then(() => res.status(200).json({ message: "Livre modifié" }))
+              .catch((error) => res.status(401).json({ error }));
+          });
+        } else {
+          Book.updateOne(
+            { _id: req.params.id },
+            { ...bookObject, _id: req.params.id }
+          )
+            .then(() => res.status(200).json({ message: "Livre modifié" }))
+            .catch((error) => res.status(401).json({ error }));
+        }
       }
     })
     .catch((error) => res.status(400).json({ error }));
